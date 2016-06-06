aeb9c9de864415dc967bac342aba3e7833759e33
Brian Rak <brakbusiness@gmail.com>
2016-06-06 03:28:24 +0000

    Formatting

--- include/cpu.h
+++ include/cpu.h
@@ -24,10 +24,10 @@ public:
   // Registers.
   typedef struct registers {
     uint8_t acc;  // Holds one of the operands and generally the result.
-	  uint8_t x, y; // Used mainly for forming effective addresses and loop counters.
-	  uint8_t p;    // Conditional register containing flags, conditions modes, etc.
-	  uint8_t sp;   // Stack pointer.
-	  uint16_t pc;   // Program counter.
+    uint8_t x, y; // Used mainly for forming effective addresses and loop counters.
+    uint8_t p;    // Conditional register containing flags, conditions modes, etc.
+    uint8_t sp;   // Stack pointer.
+    uint16_t pc;   // Program counter.
   } registers;
 
   void power(bool on);
