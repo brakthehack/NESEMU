82e2fa89f56660a15a307c793217477c99ea5c37
brakthehack <brakbusiness@gmail.com>
2016-06-09 00:01:18 -0500

    Updated CPU flag accessors

--- include/cpu6502.h
+++ include/cpu6502.h
@@ -14,17 +14,28 @@ public:
     uint8_t x, y; // Used mainly for forming effective addresses and loop counters.
     uint8_t p;    // Conditional register containing flags, conditions modes, etc.
     uint8_t sp;   // Stack pointer.
-    uint16_t pc;   // Program counter.
+    uint16_t pc;  // Program counter.
   } registers;
 
-  bool carry()    { reg.p & 0x01 != 0; }
-  bool zero()     { reg.p & 0x02 != 0; }
-  bool irq()      { reg.p & 0x04 != 0; }
-  bool decimal()  { reg.p & 0x08 != 0; }
-  bool brk()      { reg.p & 0x10 != 0; }
-  // 0x20 not used.
-  bool overflow() { reg.p & 0x40 != 0; }
-  bool negative() { reg.p & 0x80 != 0; }
+  inline bool carry()    { reg.p & 0x01 != 0; }
+  inline bool zero()     { reg.p & 0x02 != 0; }
+  inline bool irq()      { reg.p & 0x04 != 0; }
+  inline bool decimal()  { reg.p & 0x08 != 0; }
+  inline bool brk()      { reg.p & 0x10 != 0; }
+  // 0x20 flag is not used.  It is used by stack copy.
+  inline bool overflow() { reg.p & 0x40 != 0; }
+  inline bool negative() { reg.p & 0x80 != 0; }
+
+  // A set flag indicates a bit set at the appropriately described position.
+  // An unset flag will result in the bit being cleared.
+  inline void carry(bool set)    { set ? reg.p |= 0x01 : reg.p &= ~0x01; }
+  inline void zero(bool set)     { set ? reg.p |= 0x02 : reg.p &= ~0x02; }
+  inline void irq(bool set)      { set ? reg.p |= 0x04 : reg.p &= ~0x04; }
+  inline void decimal(bool set)  { set ? reg.p |= 0x08 : reg.p &= ~0x08; }
+  inline void brk(bool set)      { set ? reg.p |= 0x10 : reg.p &= ~0x10; }
+  // Once again 0x20 is not used.
+  inline void overflow(bool set) { set ? reg.p |= 0x40 : reg.p &= ~0x40; }
+  inline void negative(bool set) { set ? reg.p |= 0x80 : reg.p &= ~0x80; }
 
   virtual void power(bool on);
   virtual void reset();
@@ -32,6 +43,7 @@ public:
   Cpu6502(Decoder *d, Mmu *m) :
     Cpu(d, m) {
     reg.sp = 0xff;
+    reg.p = 0x20;
   }
 
 private:
